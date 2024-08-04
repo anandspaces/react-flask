a4b3f66413c0884f8c32ccf872c02c46653530d7
anandspaces <anandvaibhaw000@gmail.com>
2024-08-04 13:43:25 +0530

    created backend and added EditModal component in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,35 @@
 import { Container, Stack, Text } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
 import UserGrid from './components/UserGrid'
+import { useState } from 'react';
 
-export default function App() {
-  return (
-    <>
-      <Stack minH={"100vh"}>
-        <Navbar />
-        <Container maxH={"1200px"} my={4}>
-          <Text
-            fontSize={{base:"3xl",md:"50"}}
-            fontWeight={"bold"}
-            letterSpacing={"2px"}
-            textTransform={"uppercase"}
-            textAlign={"center"}
-          >
-            <Text
-              as={"span"}
-              bgGradient={"linear(to-r,cyan.400,blue.500)"}
-              bgClip={"text"}
-            >
-              My Besties
-            </Text>
-          </Text>
-          <UserGrid />
-        </Container>
-      </Stack>
-      <div>App</div>
 
-    </>
+function App() {
+  const [users, setUsers] = useState([]);
 
-  )
+	return (
+		<Stack minH={"100vh"}>
+			<Navbar setUsers={setUsers} />
+
+			<Container maxW={"1200px"} my={4}>
+				<Text
+					fontSize={{ base: "3xl", md: "50" }}
+					fontWeight={"bold"}
+					letterSpacing={"2px"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					mb={8}
+				>
+					<Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
+						My Besties
+					</Text>
+					🚀
+				</Text>
+
+				<UserGrid users={users} setUsers={setUsers} />
+			</Container>
+		</Stack>
+	);
 }
+
+export default App;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,56 +1,44 @@
 import { Box, Button, Container, Flex, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import {IoMoon} from "react-icons/io5"
 import {LuSun} from "react-icons/lu"
+import CreateUserModal from "./CreateUserModal"
+import explodeImg from ".././assets/explode.png"
+import reactImg from ".././assets/react.png"
+import pythonImg from ".././assets/python.png"
 
-export default function Navbar() {
-  const { colorMode, toggleColorMode } = useColorMode()
-  return (
-    <>
-    <Container maxW={"900px"}>
-      <Box
-        px={4}
-        my={4}
-        borderRadius={5}
-        bg={useColorModeValue("gray.200","gray.700")}
-      >
-        <Flex h={16}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-        >
-          <Flex
-            alignItems={"center"}
-            justifyContent={"center"}
-            gap={3}
-            display={{base:"none",sm:"flex"}}
-          >
-            <img src="/vite.svg" alt="image" width={50} height={50}/>
-            <Text fontSize={"40px"}>
-            +
-            </Text>
-            <img src="/vite.svg" alt="image" width={50} height={50}/>
-            <Text fontSize={"40px"}>
-            =
-            </Text>
-            <img src="/vite.svg" alt="image" width={50} height={50}/>
-          </Flex>
-          <Flex
-            gap={3}
-            alignItems={"center"}
-            >
-            <Text
-              fontSize={"lg"}
-              fontWeight={500}
-              display={{base:"none",md:"block"}}  
-            >
-              BFFShip
-            </Text>
-            <Button onClick={toggleColorMode}>
-              {colorMode === "light" ? <IoMoon /> :<LuSun size={20} />}
-            </Button>
-          </Flex>
-        </Flex>
-      </Box>
-    </Container>
-    </>
-  )
+export default function Navbar({setUsers}) {
+  const { colorMode, toggleColorMode } = useColorMode();
+	return (
+		<Container maxW={"900px"}>
+			<Box px={4} my={4} borderRadius={5} bg={useColorModeValue("gray.200", "gray.700")}>
+				<Flex h='16' alignItems={"center"} justifyContent={"space-between"}>
+					{/* Left side */}
+					<Flex
+						alignItems={"center"}
+						justifyContent={"center"}
+						gap={3}
+						display={{ base: "none", sm: "flex" }}
+					>
+						<img src={reactImg} alt='React logo' width={50} height={50} />
+						<Text fontSize={"40px"}>+</Text>
+						<img src={pythonImg} alt='Python logo' width={50} height={40} />
+						<Text fontSize={"40px"}>=</Text>
+
+						<img src={explodeImg} alt='Explode head' width={45} height={45} />
+					</Flex>
+					{/* Right side */}
+					<Flex gap={3} alignItems={"center"}>
+						<Text fontSize={"lg"} fontWeight={500} display={{ base: "none", md: "block" }}>
+							BFFship 🔥
+						</Text>
+
+						<Button onClick={toggleColorMode}>
+							{colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+						</Button>
+						<CreateUserModal setUsers={setUsers} />
+					</Flex>
+				</Flex>
+			</Box>
+		</Container>
+	);
 }
